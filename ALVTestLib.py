b089ddcbbaab94e4d4e71774f71281e98007d557
Motorscooter <Sdownard88@gmail.com>
2020-10-27 22:25:43 -0600

    Update ALVLib
    Added dataframe analysis for Diadem reader.
    self.Data will now hold all the calculated data in one dataframe for easier analyzation.

--- ALVTestLib.py
+++ ALVTestLib.py
@@ -66,6 +66,7 @@ import os
 from scipy.signal import filtfilt
 import struct
 import numpy as np
+import pandas as pd
 
 
 class ImpaxRead:
@@ -127,40 +128,34 @@ class ImpaxRead:
                 self.file_type = "Tank 02"
         else:
             self.file_type = ""
-            
-        with open(direct, 'rb') as fup:
+        self.header = {"Filename" : filename}
+        with open(direct, encoding="latin1") as fup:
             read_flag = True
             data_pts_flag = True
             while read_flag:
                 line = fup.readline()
-                if bytes('VERTSCALE', 'latin-1') in line:
-                    self.vScale = float(line[10:-1].rstrip())
-                if bytes('VERTOFFSET', 'latin-1') in line:
-                    self.vOffset = float(line[11:-1].rstrip())
-                if bytes('VERTUNITS', 'latin-1') in line:
-                    self.vUnits = line[10:-1].rstrip()
-                if bytes('HORZSCALE', 'latin-1') in line:
-                    self.hScale = float(line[10:-1].rstrip())
-                if bytes('HORZOFFSET', 'latin-1') in line:
-                    self.hOffset = float(line[11:-1].rstrip())
-                if bytes('HORZUNITS', 'latin-1') in line:
-                    self.hUnits = line[10:-1].rstrip()
-                if bytes('HUNITPERSEC', 'latin-1') in line:
-                    self.hUPS = float(line[12:-1].rstrip())
-                if bytes('RECLEN', 'latin-1') in line and data_pts_flag:
-                    data_pts_flag = False
-                    self.numDataPts = int(line[7:-1].rstrip())
-                if bytes('CLOCK_RATE', 'latin-1') in line:
-                    self.sampleRate = float(line[11:-1].rstrip())
-                if bytes('DDR_RATE', 'latin-1') in line:
-                    self.sampleRate = float(line[9:-1].rstrip())
-                if bytes('InflatorID', 'latin-1') in line:
-                    self.sampleID = (line[11:-1].rstrip()).decode()
-                if bytes('DATATYPE', 'latin-1') in line:
-                    self.data_type = (line[9:-1].rstrip()).decode('latin-1')
-                if bytes('BuildSheet', 'latin-1') in line:
-                    self.BS = line[11:-1].rstrip()
-                    read_flag = False
+                if "=" in line:
+                    line = line.strip()
+                    split_line = line.split("=")
+                    self.header[split_line[0]] = self.parse_header_data(split_line[1].strip())
+                    if 'BuildSheet' in line:
+                        read_flag = True
+            fup.close()
+            self.BS = str(self.header['BuildSheet'])
+            self.sampleID = str(self.header['Sample'])
+            self.vScale = self.header['VERTSCALE']
+            self.vOffset = self.header['VERTOFFSET']
+            self.vUnits = self.header['VERTUNITS']
+            self.hOffset = self.header['HORZOFFSET']
+            self.hScale = self.header['HORZSCALE']
+            self.hUPS = self.header['HUNITPERSEC']
+            self.numDataPts = int(self.header['RECLEN'])
+            self.data_type = self.header['DATATYPE']
+            self.test_group = self.header['TestGroup']
+            if 'CLOCK_RATE' in self.header:
+                self.sampleRate = self.header['CLOCK_RATE']
+            elif 'DDR_RATE' in self.header:
+                self.sampleRate = self.header['DDR_RATE']
             fup.close()
 
     def vert_data_points(self, sae_filter=0):
@@ -186,10 +181,100 @@ class ImpaxRead:
         time = np.arange(self.hOffset, ((self.numDataPts*self.hScale)+self.hOffset), self.hScale)
         return time
 
+    def parse_header_data(self, dataString): # will turn a string into a float if possible.
+        try:
+            data = float(dataString.strip())
+            return data
+        except ValueError:
+            return dataString
 
 class DiademRead:
-    x = 0
- 
+    """
+    1, OS
+    2, DiaDem Version
+    105, Time
+    110, Test Date
+    Local Channel Header
+    200, Channel Name
+    201, Description
+    202, Units
+    210, Channel Type
+    211, Source File
+    213, Channel
+    214, Data Type
+    220, Number of Values for Data
+    221, Pointer to first value in data
+    240, Start Value offset
+    241, Scale factor for Offset
+    250, Min Value
+    251, Max Value
+    252, No Values
+    253, Monotonous
+    260, Display data type
+    261, Visibility
+    262, Display Order
+    263, Content Width
+    265, Property Width """
+
+
+    def __init__(self, direct):
+        filename, file_ext = os.path.splitext(direct)
+        location, file = os.path.split(direct)
+        self.file = file
+        self.loca = location
+        file_ext = file_ext.strip('.')
+     #Enter error handling code
+     #Read .DAT file to build dictionary with needed data from above
+        with open(direct,'r') as fup:
+            self.fileName = filename
+            self.channel = {}
+            channel_header = False
+            for line in fup:
+                if channel_header:
+                    linetxt = line.split(',')
+                    if linetxt[0] == '200':
+                        self.channel[linetxt[1].rstrip()] = {}
+                        channelname = linetxt[1].rstrip()
+                    elif linetxt[0] == '201':
+                        self.channel[channelname]['Description'] = linetxt[1].rstrip()
+                    elif linetxt[0] == '202':
+                        self.channel[channelname]['Units'] = linetxt[1].rstrip()
+                    elif linetxt[0] == '211':
+                        self.channel[channelname]['R32'] = (self.loca + '\\' + linetxt[1]).rstrip()
+                    elif linetxt[0] == '214':
+                        self.channel[channelname]['Data Type'] = linetxt[1].rstrip()
+                    elif linetxt[0] == '220':
+                        self.channel[channelname]['NumDatPts'] = int(linetxt[1].rstrip())
+                    elif linetxt[0] == '221':
+                        self.channel[channelname]['DataLoc'] = int(linetxt[1].rstrip())
+                    elif linetxt[0] == '240':
+                        self.channel[channelname]['OffSet'] = float(linetxt[1].rstrip())
+                    elif linetxt[0] == '241':
+                        self.channel[channelname]['Scale'] = float(linetxt[1].rstrip())
+                    elif linetxt[0] == '250':
+                        self.channel[channelname]['Min'] = float(linetxt[1].rstrip())
+                    elif linetxt[0] == '251':
+                        self.channel[channelname]['Max'] = float(linetxt[1].rstrip())
+
+                if 'BEGINCHANNELHEADER' in line:
+                    channel_header = True
+                elif 'ENDCHANNGELHEADER' in line:
+                    channel_header = False
+                    channelname = None
+            fup.close()
+        data_type_dict = dict(REAL32=['f', 4], SHORT=['h', 2], LONG=['l', 4])
+        key_file = list(self.channel.keys())[-1]
+        self.data = pd.DataFrame()
+        with open(self.channel[key_file]['R32'], 'rb') as fup:
+            size = fup.read()
+            data_array = struct.unpack('f' * int(len(size) / 4), size)
+            for key in self.channel.keys():
+                raw_data = data_array[self.channel[key]['DataLoc']-1:self.channel[key]['NumDatPts']+self.channel[key]['DataLoc']-1]
+                raw_array = np.asarray(raw_data)
+                float_array = raw_array.astype(float)
+                calc_data = float_array * self.channel[key]['Scale'] + self.channel[key]['OffSet']
+                self.data[key] = pd.Series(calc_data)
+                fup.close()
 
 def filter_processing(data, cfc, sample_rate):
     # calculate sample rate
@@ -212,3 +297,25 @@ def filter_processing(data, cfc, sample_rate):
 #    b,a = butter(2,wn,'low')
     y = filtfilt(b, a, data)
     return y
+
+# def linear_analysis(impactor_distance, dist_data, accel_data, time):
+#     bottomOut = False
+#     max_displacement = max(dist_data)
+#     if impactor_distance < max_displacement:
+#         bottomOut = True
+#     if bottomOut:
+#
+#     max_accel = min(accel_data)
+
+
+def filter_analysis(data, sample_rate, cfc_list = None ):
+    filter_analysis_dict = {}
+    if cfc_list is None:
+        cfc_list = [0,60,180,1000,5000]
+    for i in cfc_list:
+        if i == 0:
+            filter_analysis_dict['RAW'] = filter_processing(data, i, sample_rate)
+        else:
+            filter_analysis_dict[str(i)] = filter_processing(data, i, sample_rate)
+    return filter_analysis_dict
+
